f64675e8108d3009858c9456116bb9b26d272ffc
Janders1800 <zenit1800@gmail.com>
2023-08-20 14:41:11 +0200

    Fixed transforms bug wen merger is not at world origin.

--- MeshMerger.gd
+++ MeshMerger.gd
@@ -9,26 +9,23 @@ export (bool) var delete_child_meshes_on_play: bool
 export var generate_collisions: bool = false
 
 
-var material_counter: int = 0
+var material_counter: int
 var collision_node: StaticBody
 
 
-func _ready():
+func _ready() -> void:
 	if delete_child_meshes_on_play:
 		for node in get_children():
-			if node is StaticBody:
-				pass
-			else:
+			if not node is StaticBody:
 				node.queue_free()
 
 
 # warning-ignore:unused_argument
-func bake_meshes(value):
+func bake_meshes(value: bool) -> void:
 	if !Engine.editor_hint:
 		return
 	
-	if mesh != null:
-		mesh = null
+	mesh = null
 	
 	if generate_collisions:
 		collision_node = StaticBody.new()
@@ -50,35 +47,60 @@ func bake_meshes(value):
 				new_mesh = extract_mesh(nodeChild, new_mesh)
 				generate_collison(nodeChild)
 	
+	new_mesh = combine_materials(new_mesh)
+	
 	set_children_visibility(false)
 	self.mesh = new_mesh
 
 
 # warning-ignore:unused_argument
-func clean_meshes(value):
+func clean_meshes(value: bool) -> void:
 	if !Engine.editor_hint:
 		return
-
+	
 	mesh = null
 	clean_collisions()
 	set_children_visibility(true)
 
 
-func extract_mesh(node: MeshInstance, new_mesh: ArrayMesh):
-	for i in node.mesh.get_surface_count():
-		var data = node.mesh.surface_get_arrays(i)
+func extract_mesh(node: MeshInstance, new_mesh: ArrayMesh) -> ArrayMesh:
+	for i in range(node.mesh.get_surface_count()):
+		var data: Array = node.mesh.surface_get_arrays(i)
 		var offset: PoolVector3Array = data[ArrayMesh.ARRAY_VERTEX]
 		
-		for o in offset.size():
-			offset.set(o, node.global_transform.basis.xform(offset[o]) + node.global_transform.origin)
+		for o in range(offset.size()):
+			offset.set(o, node.global_transform.basis.xform(offset[o]) + node.global_transform.origin - global_transform.origin)
 		
 		data[ArrayMesh.ARRAY_VERTEX] = offset
 		
 		new_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, data)
 		new_mesh.surface_set_material(material_counter, node.mesh.surface_get_material(i))
-		material_counter +=1
+		material_counter += 1
 		
-		return new_mesh
+	return new_mesh
+
+
+func combine_materials(mesh: ArrayMesh) -> ArrayMesh:
+	var result_mesh := ArrayMesh.new()
+	var mat_counter := 0
+	var materials: Array
+	
+	for i in range(mesh.get_surface_count()):
+		if not materials.has(mesh.surface_get_material(i)):
+			materials.append(mesh.surface_get_material(i))
+	
+	for mat in materials:
+		var surf_tool := SurfaceTool.new()
+		
+		for i in range(mesh.get_surface_count()):
+			if mat == mesh.surface_get_material(i):
+				surf_tool.append_from(mesh, i, Transform.IDENTITY)
+		
+		result_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surf_tool.commit_to_arrays())
+		result_mesh.surface_set_material(mat_counter, mat)
+		mat_counter += 1
+	
+	return result_mesh
 
 
 func generate_collison(node):
@@ -90,18 +112,19 @@ func generate_collison(node):
 				if grandchild is CollisionShape:
 					var new_col := CollisionShape.new()
 					new_col.global_transform = child.global_transform
+					new_col.transform.origin = new_col.transform.origin - global_transform.origin
 					collision_node.add_child(new_col)
 					new_col.shape = grandchild.shape
 					new_col.set_owner(get_tree().get_edited_scene_root())
 
 
-func clean_collisions():
+func clean_collisions() -> void:
 	for child in get_children():
 		if child is StaticBody:
 			child.queue_free()
 
 
-func set_children_visibility(value):
+func set_children_visibility(value: bool) -> void:
 	children_visibility = value
 	for node in get_children():
 		if not node is StaticBody:
